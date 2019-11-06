cd786e5872b29f4b9d4093d3857147d50c4b69c2
Qanat <kmergenbayev@gmail.com>
2019-11-06 13:04:48 +0600

    add consts.

---
+++ const.py
@@ -0,0 +1,18 @@
+
+gemini_data_source = {"BTCUSD": ["gemini_BTCUSD_2015_1min.csv",
+                                 "gemini_BTCUSD_2016_1min.csv",
+                                 "gemini_BTCUSD_2017_1min.csv",
+                                 "gemini_BTCUSD_2018_1min.csv",
+                                 "gemini_BTCUSD_2019_1min.csv"],
+                      "ETHUSD": ["gemini_ETHUSD_2016_1min.csv",
+                                 "gemini_ETHUSD_2017_1min.csv",
+                                 "gemini_ETHUSD_2018_1min.csv",
+                                 "gemini_ETHUSD_2019_1min.csv"],
+                      "LTCUSD": ["gemini_LTCUSD_2018_1min.csv",
+                                 "gemini_LTCUSD_2019_1min.csv"],
+                      "ZECUSD": ["gemini_ZECUSD_2018_1min.csv",
+                                 "gemini_ZECUSD_2019_1min.csv"]}
+
+cross_symbols = {"XRPBTC": {"long": "XRPUSD", "short": "BTCUSD"},
+                 "ETHBTC": {"long": "ETHUSD", "short": "BTCUSD"},
+                 "XRPETH": {"long": "XRPUSD", "short": "ETHUSD"}}
