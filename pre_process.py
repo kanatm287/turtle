618092874b67f13e78b45e6513aee333f7f6fdbf
Qanat <kmergenbayev@gmail.com>
2019-11-06 13:05:14 +0600

    add gemini datasource.

--- pre_process.py
+++ pre_process.py
@@ -1,49 +1,94 @@
 import pandas as pd
+import numpy as np
+import functools
+import const
 import load
 import os
 
 
-def minute_data(symbol, days):
+def gemini_data(data_frame, file_name):
 
-    if not os.path.isfile("./" + symbol + ".csv"):
+    minute_dataframe = pd.read_csv("./gemini/" + file_name)\
+        .rename(columns={"Date": "date",
+                         "Open": "open",
+                         "High": "high",
+                         "Low": "low",
+                         "Close": "close",
+                         "Volume": "volume"})
 
-        minute_dataframe = load.exchange_historical_data(symbol, days)
+    return pd.concat([data_frame, pd.DataFrame(minute_dataframe)])
 
-        minute_dataframe = minute_dataframe.resample("1min").mean()
 
-        minute_dataframe.fillna(method="ffill", inplace=True)
+def process_data(minute_dataframe):
 
-        minute_dataframe["date_string"] = minute_dataframe.index.strftime('%Y-%m-%d')
+    minute_dataframe = minute_dataframe.resample("1min").mean()
 
-        minute_dataframe.reset_index(level=0, inplace=True)
+    minute_dataframe.fillna(method="ffill", inplace=True)
 
-        minute_dataframe = minute_dataframe.merge(minute_dataframe
-                                                  .groupby("date_string")
-                                                  .size()
-                                                  .to_frame("count")
-                                                  .reset_index(),
-                                                  on="date_string",
-                                                  how='inner')
+    minute_dataframe["date_string"] = minute_dataframe.index.strftime('%Y-%m-%d')
 
-        minute_dataframe = minute_dataframe.loc[minute_dataframe["count"] == 1440]
+    minute_dataframe.reset_index(level=0, inplace=True)
 
-        minute_dataframe = minute_dataframe.drop(columns=["date_string", "count"])
+    minute_dataframe = minute_dataframe.merge(minute_dataframe
+                                              .groupby("date_string")
+                                              .size()
+                                              .to_frame("count")
+                                              .reset_index(),
+                                              on="date_string",
+                                              how='inner')
 
-        minute_dataframe.set_index("date", inplace=True)
+    minute_dataframe = minute_dataframe.loc[minute_dataframe["count"] == 1440]
 
-        minute_dataframe.sort_index(inplace=True)
+    minute_dataframe = minute_dataframe.drop(columns=["date_string", "count"])
 
-        minute_dataframe.to_csv("./" + symbol + ".csv", header=True)
+    minute_dataframe.set_index("date", inplace=True)
+
+    minute_dataframe.sort_index(inplace=True)
+
+    return minute_dataframe
+
+
+def minute_data(symbol, days, data_source):
+
+    if data_source == "gemini":
+
+        minute_dataframe = functools.reduce(lambda a, b: gemini_data(a, b),
+                                            const.gemini_data_source[symbol],
+                                            pd.DataFrame())
+
+        minute_dataframe["date"] = np.where(minute_dataframe["date"] < 9999999999,
+                                            minute_dataframe["date"] * 1000,
+                                            minute_dataframe["date"])
+
+        minute_dataframe["date"] = pd.to_datetime(minute_dataframe["date"], unit="ms")
+
+        minute_dataframe = minute_dataframe.set_index(pd.DatetimeIndex(minute_dataframe["date"]))
+
+        minute_dataframe = minute_dataframe.drop(columns=["date"])
+
+        minute_dataframe = process_data(minute_dataframe)
 
         return minute_dataframe
 
     else:
 
-        minute_dataframe = pd.read_csv("./" + symbol + ".csv")
+        if not os.path.isfile("./" + symbol + ".csv"):
 
-        minute_dataframe = minute_dataframe.set_index(pd.DatetimeIndex(minute_dataframe["date"]))
+            minute_dataframe = load.exchange_historical_data(symbol, days)
 
-        return minute_dataframe.drop(columns=["date"])
+            minute_dataframe = process_data(minute_dataframe)
+
+            minute_dataframe.to_csv("./" + symbol + ".csv", header=True)
+
+            return minute_dataframe
+
+        else:
+
+            minute_dataframe = pd.read_csv("./" + symbol + ".csv")
+
+            minute_dataframe = minute_dataframe.set_index(pd.DatetimeIndex(minute_dataframe["date"]))
+
+            return minute_dataframe.drop(columns=["date"])
 
 
 def rename_columns(data_frame, symbol):
@@ -60,10 +105,18 @@ def rename_columns(data_frame, symbol):
                                                                      symbol + "_volume"]]
 
 
-def synthetic_minute_data(long_param, short_param):
+def synthetic_minute_data(long_param, short_param, data_source):
 
-    long_dataframe = rename_columns(pd.read_csv("./" + long_param + ".csv"), long_param)
-    short_dataframe = rename_columns(pd.read_csv("./" + short_param + ".csv"), short_param)
+    if data_source == "gemini":
+        long_dataframe = functools.reduce(lambda a, b: gemini_data(a, b),
+                                          const.gemini_data_source[long_param],
+                                          pd.DataFrame())
+        short_dataframe = functools.reduce(lambda a, b: gemini_data(a, b),
+                                           const.gemini_data_source[short_param],
+                                           pd.DataFrame())
+    else:
+        long_dataframe = rename_columns(pd.read_csv("./" + long_param + ".csv"), long_param)
+        short_dataframe = rename_columns(pd.read_csv("./" + short_param + ".csv"), short_param)
 
     combined_dataframe = pd.merge(long_dataframe, short_dataframe, on="date")
 
